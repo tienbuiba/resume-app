5cc8566862cd550ca36d314344bb00259f40284d
tienbuiba <buibatien2408@gmail.com>
2023-09-08 12:13:25 +0700

    add init project

---
+++ data/page-data.jsx
@@ -0,0 +1,67 @@
+export const personalData = {
+	name: "BUI BA TIEN",
+	role: "Software Engineer",
+	education: [
+		"Software Engineering, Hanoi University of Science and Technology(2018-2023)"
+	],
+	contactLinks: [
+		"mailto:buibatien2408@gmail.com",
+		"https://twitter.com/time_tocode",
+		"https://www.linkedin.com/in/tien-bui-4b09b3212/",
+		"https://medium.com/@time2code.ali",
+	],
+};
+
+export const aboutMe = {
+	title: "About Me",
+	body: [
+		`As an enthusiastic and determined individual driven by a deep desire to create a positive influence, I possess a distinctive combination of technical expertise and interpersonal skills, stemming from my background in computer science. I excel at addressing intricate challenges effortlessly and remain unwavering in my pursuit of ultimate objectives, even in the face of adversity.
+
+		Outside of my professional commitments, I find joy in hiking, indulging in literature, and cherishing moments with my close-knit circle of friends and family. My unwavering resolve, proactive outlook, and strong work ethic establish me as a valuable asset to any team.`,
+	],
+};
+
+export const skills = {
+	soft: [
+		{ icon: "👂🏼", text: "Active Listening" },
+		{ icon: "💬", text: "Effective Communication" },
+		{ icon: "👥", text: "Collaboration" },
+		{ icon: "⽓", text: "Teamwork" },
+		{ icon: "💡", text: "Creative Problem Solving" },
+		{ icon: "⌛️", text: "Time management" },
+	],
+	hard: [
+		{ icon: "js", text: "JavaScipt" },
+		{ icon: "ʦ", text: "TypeScript" },
+		{ icon: "🚀", text: "React" },
+		{ icon: "🚀", text: "React Native" },
+		{ icon: "💾", text: "SQL" },
+		{ icon: "💾", text: "noSQL" },
+		{ icon: "📈", text: "Data Structures and Algorithms" },
+	],
+};
+
+export const professionalData = {
+	title: "Professional Experience",
+	experiences: [
+		{
+			role: "Data Scientist, Acme Inc. (2021-Present)",
+			description:
+				"Design and implement machine learning models to analyze large datasets and drive business decisions. Collaborate with cross-functional teams to develop and launch new products and features. Provide technical guidance and mentorship to junior data scientists. Conduct regular presentations to stakeholders on the findings and insights generated from data analysis.",
+			current: true,
+		},
+
+		{
+			role: "Machine Learning Engineer, XYZ Corp. (2019-2021)",
+			description:
+				"Designed and implemented advanced machine learning algorithms to improve predictive accuracy and performance. Built and maintained scalable machine learning infrastructure using cloud computing platforms. Conducted data exploratory analysis to identify potential use cases and opportunities. Participated in regular code reviews to maintain high quality standards.",
+			current: false,
+		},
+		{
+			role: "Machine Learning Engineer, ABX Corp. (2020-2021)",
+			description:
+				"Designed and implemented advanced machine learning algorithms to improve predictive accuracy and performance. Built and maintained scalable machine learning infrastructure using cloud computing platforms. Conducted data exploratory analysis to identify potential use cases and opportunities. Participated in regular code reviews to maintain high quality standards.",
+			current: false,
+		},
+	],
+};
